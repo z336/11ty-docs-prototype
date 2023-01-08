1c266570594df992070141b4c0a2bf07caf46ea7
Jon Coleman <jcoleman1388@gmail.com>
2023-01-08 00:24:01 -0500

    mobile nav wip

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -31,38 +31,38 @@ if (currentPage) {
 //   tableOfContents.insertAdjacentElement("afterbegin", "<p>hey</p>");
 // }
 
-// // Mobile nav menu
+// Mobile nav menu
 
-// const nav = document.querySelector("nav");
-// const list = nav.querySelector("ul");
-// const burgerClone = document.querySelector("#burger-template").content.cloneNode(true);
-// const svg = nav.querySelector("svg");
+const nav = document.querySelector(".navbar");
+const list = nav.querySelector("ul");
+const burgerClone = document.querySelector("#burger-template").content.cloneNode(true);
+const svg = nav.querySelector("svg");
 
-// const button = burgerClone.querySelector("button");
-// button.addEventListener("click", (event) => {
-//   const isOpen = button.getAttribute("aria-expanded") === "false";
-//   button.setAttribute("aria-expanded", isOpen);
-// });
+const button = burgerClone.querySelector("button");
+button.addEventListener("click", (event) => {
+  const isOpen = button.getAttribute("aria-expanded") === "false";
+  button.setAttribute("aria-expanded", isOpen);
+});
 
-// // avoid DRY: disabling menu
-// const disableMenu = () => {
-//   button.setAttribute("aria-expanded", false);
-//   button.focus();
-// };
+// avoid DRY: disabling menu
+const disableMenu = () => {
+  button.setAttribute("aria-expanded", false);
+  button.focus();
+};
 
-// //  close on escape
-// nav.addEventListener("keyup", (event) => {
-//   if (event.code === "Escape") {
-//     disableMenu();
-//   }
-// });
+//  close on escape
+nav.addEventListener("keyup", (event) => {
+  if (event.code === "Escape") {
+    disableMenu();
+  }
+});
 
-// // close if clicked outside of event target
-// document.addEventListener("click", (event) => {
-//   const isClickInsideElement = nav.contains(event.target);
-//   if (!isClickInsideElement) {
-//     disableMenu();
-//   }
-// });
+// close if clicked outside of event target
+document.addEventListener("click", (event) => {
+  const isClickInsideElement = nav.contains(event.target);
+  if (!isClickInsideElement) {
+    disableMenu();
+  }
+});
 
-// nav.insertBefore(burgerClone, list);
+nav.insertBefore(burgerClone, list);

ddef4c540eb88e55c1859a20b0d5018e634ffa07
Jon Coleman <jcoleman1388@gmail.com>
2023-01-09 01:03:40 -0500

    removed unused shortcode

--- config/shortcodes/index.js
+++ config/shortcodes/index.js
@@ -3,6 +3,5 @@ const contact = require("./contact");
 const details = require("./details");
 const image = require("./image");
 const search = require("./search");
-const tableOfContents = require("./tableOfContents");
 
-module.exports = { callout, contact, details, image, search, tableOfContents };
+module.exports = { callout, contact, details, image, search };
--- config/shortcodes/tableofcontents/index.js
+++
@@ -1,7 +0,0 @@
-const { html } = require("code-tag");
-
-function tableOfContents(content) {
-  return html`<div class="grid">${content}</div>`;
-}
-
-module.exports = tableOfContents;

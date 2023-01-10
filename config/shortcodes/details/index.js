15069f4e1fea5fff2fc00e8c683e3f2f1278f072
Jon Coleman <jcoleman1388@gmail.com>
2023-01-10 13:31:24 -0500

    small fixes

--- config/shortcodes/details/index.js
+++ config/shortcodes/details/index.js
@@ -3,7 +3,7 @@ const { html } = require("code-tag");
 function details(content, detailsClass, summary) {
   return html`<details class="${detailsClass}">
     <summary>${summary}</summary>
-    <p>${content}</p>
+    ${content}
   </details>`;
 }
 

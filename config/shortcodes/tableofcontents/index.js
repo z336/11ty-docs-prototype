054c2b918eab2a3fbea845c699768fc6269c40ad
Jon Coleman <jcoleman1388@gmail.com>
2023-01-11 14:32:05 -0500

    toc wip

---
+++ config/shortcodes/tableofcontents/index.js
@@ -0,0 +1,7 @@
+const { html } = require("code-tag");
+
+function tableofcontents() {
+  return html`<div>[toc]</div>`;
+}
+
+module.exports = tableofcontents;
--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,9 +1,9 @@
 // Append the table of contents heading to avoid mapping it to its own list
 
-const toc = document.querySelector(".table-of-contents");
+const tableOfContents = document.querySelector(".table-of-contents");
 
-if (document.body.contains(toc)) {
-  toc.insertAdjacentHTML("afterbegin", "<h2>On this page</h2>");
+if (document.body.contains(tableOfContents)) {
+  tableOfContents.insertAdjacentHTML("afterbegin", "<h2>On this page</h2>");
 }
 
 // Toggle details in left navbar to stay open for current page

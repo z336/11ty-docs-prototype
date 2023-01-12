ce5404c8fd4a9862e5a4cabb86e3ca5b721b6987
Jon Coleman <jcoleman1388@gmail.com>
2023-01-11 19:16:04 -0500

    more safari reader fixes

--- src/includes/components/tableofcontents/index.js
+++ src/includes/components/tableofcontents/index.js
@@ -3,7 +3,7 @@ const { html } = require("code-tag");
 // white space around toc is necessary for it to render
 function tableofcontents() {
   return html`<div class="table-of-contents">
-    <h2>On this page</h2>
+    <h2><span>On this page</span></h2>
     
     [toc]
 
